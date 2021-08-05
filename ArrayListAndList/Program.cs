5b66d1505e6bf11f71fcc815686ec5200eb3d77e
saiful Islam <saifulislam0980@gmail.com>
2021-08-06 01:42:42 +0600

    Updated

--- ArrayListAndList/Program.cs
+++ ArrayListAndList/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,41 @@ namespace ArrayListAndList
     {
         static void Main(string[] args)
         {
+            //ArrayList
+            ArrayList array=new ArrayList();
+            array.Add(20);
+            array.Add(30);
+            array.Add(50);
+            array.Add(60);
+            array.Add(10);
+            array.Add(15);
+            array.Add(40);
+            array.Add(90);
+            array.Add(60);
+            array.Add(100);
+            array.Add(120);
+            array.RemoveAt(9);
+            foreach (var value in array)
+            {
+                Console.WriteLine(value);
+            }
+
+
+            //List
+            List<string> names=new List<string>();
+            names.Add("Saiful");
+            names.Add("Islam");
+            names.Add("Pavel");
+            names.Add("Mou");
+            names.ForEach(Console.WriteLine);
+            foreach (var data in names)
+            {
+                if (data=="Saiful")
+                {
+                    Console.WriteLine("Found");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
